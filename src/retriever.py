93cdc66c856921a72c01ee61e3dbee47b807127d
imgopal0101-crypto <imgopal0101@gmail.com>
2026-08-03 22:30:35 +0530

    Enable auto FAISS creation

--- src/retriever.py
+++ src/retriever.py
@@ -1,49 +1,33 @@
+import os
 from langchain_community.vectorstores import FAISS
+
 from src.config import FAISS_PATH
 from src.embeddings import get_embedding_model
-import streamlit as st
-import os
+from src.vector_store import create_vector_store
 
 
 def get_retriever():
-    """
-    Load the FAISS vector database and return a retriever.
-    """
+
+    # Agar FAISS database nahi hai to automatically create karo
+    if (
+        not os.path.exists(FAISS_PATH)
+        or not os.path.exists(os.path.join(FAISS_PATH, "index.faiss"))
+        or not os.path.exists(os.path.join(FAISS_PATH, "index.pkl"))
+    ):
+        create_vector_store()
 
     embedding_model = get_embedding_model()
 
-    # Debug Information
-    st.write("### Debug Information")
-    st.write("Current Working Directory:", os.getcwd())
-    st.write("FAISS Path:", FAISS_PATH)
-    st.write("FAISS Folder Exists:", os.path.exists(FAISS_PATH))
-    st.write(
-        "index.faiss Exists:",
-        os.path.exists(os.path.join(FAISS_PATH, "index.faiss"))
-    )
-    st.write(
-        "index.pkl Exists:",
-        os.path.exists(os.path.join(FAISS_PATH, "index.pkl"))
+    vector_db = FAISS.load_local(
+        FAISS_PATH,
+        embedding_model,
+        allow_dangerous_deserialization=True,
     )
 
-    try:
-        vector_db = FAISS.load_local(
-            FAISS_PATH,
-            embedding_model,
-            allow_dangerous_deserialization=True,
-        )
-
-        st.success("✅ FAISS Vector Database Loaded Successfully")
-
-        return vector_db.as_retriever(
-            search_type="mmr",
-            search_kwargs={
-                "k": 10,
-                "fetch_k": 20,
-            },
-        )
-
-    except Exception as e:
-        st.error("❌ Failed to load FAISS Vector Database")
-        st.exception(e)
-        raise
+    return vector_db.as_retriever(
+        search_type="mmr",
+        search_kwargs={
+            "k": 10,
+            "fetch_k": 20,
+        },
+    )
